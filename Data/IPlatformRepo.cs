444a3f10cdb7ee6d4c60a9614eb400f913d3f3c4
Sander Wieringa <s.wieringa@student.fontys.nl>
2022-08-02 16:42:09 +0200

    .

---
+++ Data/IPlatformRepo.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+    }
+}
